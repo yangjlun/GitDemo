536a9c203b21352051ffb19c97726cc27a79584d
60164 <601642499@qq.com>
2020-04-22 13:57:45 +0800

    新建

---
+++ git001.py
@@ -0,0 +1,12 @@
+my_list = ["yjl","ss"]
+
+#for name in my_list:
+#   print(name)
+
+#for number in range(11):
+#   print(number)
+
+for letter in 'yjl':
+    print("each letter is:" + letter)
+
+    '''woshiyjl'''
